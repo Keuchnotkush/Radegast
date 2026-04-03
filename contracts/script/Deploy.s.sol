eef97f7b367f79280734884c4e86615ac571f148
manny <115085160+manny-pro@users.noreply.github.com>
2026-04-03 23:48:09 +0200

    wip: 23:48

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -18,6 +18,12 @@ contract Deploy is Script {
         XStockMock spy = new XStockMock("S&P 500 xStock", "SPYx", 530e6);
         XStockMock ndx = new XStockMock("Nasdaq xStock", "NDXx", 480e6);
         XStockMock mstr = new XStockMock("MicroStrategy xStock", "MSTRx", 1700e6);
+        XStockMock msft = new XStockMock("Microsoft xStock", "MSFTx", 420e6);
+        XStockMock jpm = new XStockMock("JPMorgan xStock", "JPMx", 240e6);
+        XStockMock visa = new XStockMock("Visa xStock", "Vx", 310e6);
+        XStockMock xom = new XStockMock("Exxon xStock", "XOMx", 115e6);
+        XStockMock lly = new XStockMock("Eli Lilly xStock", "LLYx", 780e6);
+        XStockMock lvmh = new XStockMock("LVMH xStock", "LVMHx", 750e6);
 
         address d = vm.addr(pk);
         uint256 M = tsla.MINTER_ROLE();
@@ -31,6 +37,12 @@ contract Deploy is Script {
         spy.grantRoles(d, M);
         ndx.grantRoles(d, M);
         mstr.grantRoles(d, M);
+        msft.grantRoles(d, M);
+        jpm.grantRoles(d, M);
+        visa.grantRoles(d, M);
+        xom.grantRoles(d, M);
+        lly.grantRoles(d, M);
+        lvmh.grantRoles(d, M);
 
         ConsensusSettlement cs = new ConsensusSettlement();
         cs.grantRoles(d, cs.SUBMITTER_ROLE());
